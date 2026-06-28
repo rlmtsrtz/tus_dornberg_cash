aba154c7fc7f6d05cdd1dd3383ba5b18016f5ca2
Mirco <felske.mirco@gmail.com>
2026-06-28 16:08:14 +0200

    save

--- lib/services/google_sheets_service.dart
+++ lib/services/google_sheets_service.dart
@@ -2,10 +2,10 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class GoogleSheetsService {
-  // URL https://script.google.com/macros/s/AKfycbxTPMs9d8eWOo67KqgwfvjvF--ECLc4FFd-FOwa_OvjlZcG9vrTN-On6chW-0JR8fWj/exec
+  // URL https://script.google.com/macros/s/AKfycbwl22GNvzkc51JyeTBEHSbg7raC1V9lXseojqb2ZPZnpygjKKTLoU72pkmJyAxjxfzw/exec
   // ID AKfycbxTPMs9d8eWOo67KqgwfvjvF--ECLc4FFd-FOwa_OvjlZcG9vrTN-On6chW-0JR8fWj
   // TODO: Replace this with your actual Google Apps Script Web App URL after deployment
-  static const String _scriptUrl = 'https://script.google.com/macros/s/AKfycbxTPMs9d8eWOo67KqgwfvjvF--ECLc4FFd-FOwa_OvjlZcG9vrTN-On6chW-0JR8fWj/exec';
+  static const String _scriptUrl = 'https://script.google.com/macros/s/AKfycbwl22GNvzkc51JyeTBEHSbg7raC1V9lXseojqb2ZPZnpygjKKTLoU72pkmJyAxjxfzw/exec';
 
   static Future<bool> saveNumber(int value) async {
     if (_scriptUrl == 'YOUR_APPS_SCRIPT_URL_HERE') {
